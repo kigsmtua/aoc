e53892cf80bcfd1fde240e671720a21f8f1a1d1d
John Kiragu <mutuakiragu@gmail.com>
2023-10-24 16:05:52 +0300

    day2 question one done in golang

---
+++ 2015/day2/2.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	data, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println("Please input the correct path to the file")
+	}
+	fileScanner := bufio.NewScanner(data)
+	fileScanner.Split(bufio.ScanLines)
+	var fileContents []string
+	for fileScanner.Scan() {
+		fileContents = append(fileContents, fileScanner.Text())
+	}
+
+	totalGiftWrapperSize := 0
+	for _, measurements := range fileContents {
+		measures := strings.Split(measurements, "x")
+		length, _ := strconv.Atoi(measures[0])
+		width, _ := strconv.Atoi(measures[1])
+		height, _ := strconv.Atoi(measures[2])
+		totalGiftWrapperSize += findGiftSize(length, width, height)
+	}
+	fmt.Println(totalGiftWrapperSize)
+
+}
+
+// / find surface are of a right rectangularr prism plus the area with the smallest size
+func findGiftSize(length int, width int, height int) int {
+
+	surfaceArea := (2 * length * width) + (2 * width * height) + (2 * length * height)
+	giftsize := surfaceArea + findSmallestArea(length, width, height)
+	return giftsize
+}
+
+// / Given a right rectangularr prism, find the area with the smallest size
+func findSmallestArea(length int, width int, height int) int {
+	area1 := length * width
+	area2 := length * height
+	area3 := width * height
+	min := area1
+	if area2 < min {
+		min = area2
+	}
+	if area3 < min {
+		min = area3
+	}
+	return min
+}
