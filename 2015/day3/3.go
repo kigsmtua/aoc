d1d5a3a5895280c1dc63677c732cccfbd5da9a13
John Kiragu <mutuakiragu@gmail.com>
2023-10-26 19:45:03 +0300

    day 2 solution pumped in and solved

--- 2015/day3/3.go
+++ 2015/day3/3.go
@@ -5,30 +5,55 @@ import (
 	"os"
 )
 
+var Directions map[rune]complex128
+
+func init() {
+	Directions = map[rune]complex128{
+		'^': 0 + 1i,
+		'>': 1 + 0i,
+		'v': 0 - 1i,
+		'<': -1 + 0i,
+	}
+}
+
 func main() {
 	data, err := os.ReadFile("input.txt")
 
 	if err != nil {
 		fmt.Println("Heyooo, that file is not found")
 	}
-
 	movements := string(data)
+	fmt.Println(problem1(movements))
+	fmt.Println(problem2(movements))
+}
+
+func problem1(movements string) int {
 	visited := make(map[complex128]bool)
 	position := complex(0, 0)
 	visited[position] = true
 
-	directions := map[rune]complex128{
-		'^': 0 + 1i,
-		'>': 1 + 0i,
-		'v': 0 - 1i,
-		'<': -1 + 0i,
-	}
-
 	for _, char := range movements {
-		position += directions[char]
+		position += Directions[char]
 		visited[position] = true
 	}
+	return len(visited)
+}
 
-	fmt.Println(visited)
-	fmt.Println(len(visited))
+func problem2(movements string) int {
+	visited := make(map[complex128]bool)
+	santa := complex(0, 0)
+	santaBot := complex(0, 0)
+	visited[santa] = true
+	visited[santaBot] = true
+
+	for pos, char := range movements {
+		if pos%2 == 0 {
+			santa += Directions[char]
+			visited[santa] = true
+		} else {
+			santaBot += Directions[char]
+			visited[santaBot] = true
+		}
+	}
+	return len(visited)
 }
