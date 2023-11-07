7d79aaf2e1b20750779e180c411184073824cf88
John Kiragu <mutuakiragu@gmail.com>
2023-11-07 12:45:42 +0300

    we run into the solution to the first problem

--- 2015/day4/4.go
+++ 2015/day4/4.go
@@ -3,7 +3,35 @@ package main
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"fmt"
+	"strconv"
+	"strings"
 )
+
+func main() {
+	mineAdventCoins("bgvyzdsv")
+}
+
+func mineAdventCoins(input string) bool {
+	number := 1
+	magicInput := input
+	for {
+		md5hash := getMD5Hash(magicInput + strconv.Itoa(number))
+		if startsWithFiveZeros(md5hash) {
+			fmt.Println(number)
+			fmt.Println(md5hash)
+			break
+		}
+		number += 1
+	}
+	return false
+}
+
+// We check if the string has 5 preciding zeros
+func startsWithFiveZeros(s string) bool {
+	prefix := "00000"
+	return strings.HasPrefix(s, prefix)
+}
 
 // Returns the md5 hash of a string
 func getMD5Hash(text string) string {
